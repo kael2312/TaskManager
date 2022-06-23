3f93069fe5f67a05930c78cd45ca425a24ecfd60
kael2312 <lenhathoang2311@gmail.com>
2022-06-23 07:04:30 +0700

    commit project card

--- react-app/src/features/Projects/pages/project-page.jsx
+++ react-app/src/features/Projects/pages/project-page.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import Stack from "@mui/material/Stack";
+import ProjectCard from "../components/ProjectCard/project-card";
 
-ProjectPage.propTypes = {
-    listProject: PropTypes.array
-};
+ProjectPage.propTypes = {};
 
 function ProjectPage(props) {
-    const { listProject } = props
     const listProjectState = useSelector((state) => state.project.current);
-    console.log("From props: ", listProject);
-    console.log("From state: ", listProjectState);
+    console.log(listProjectState);
     return (
-        <ul>
-            {
-                listProjectState.map(todo => (
-                    <li key={todo.projectID}>{todo.projectName}</li>
-                ))
-            }
-        </ul>
+        <Stack direction="row" spacing={2}>
+            {listProjectState.map((project) => {
+                return (
+                    <div key={project.projectID}>
+                        <ProjectCard  project={project}></ProjectCard>
+                    </div>
+                )
+            })}
+        </Stack>
     );
 }
 
--- react-app/src/features/Projects/project.jsx
+++ react-app/src/features/Projects/project.jsx
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import ProjectPage from './pages/project-page';
-import { useDispatch, useSelector } from 'react-redux';
-import { getListProject } from './projectSlice';
-import { unwrapResult } from '@reduxjs/toolkit';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import ProjectPage from "./pages/project-page";
+import { useDispatch, useSelector } from "react-redux";
+import { getListProject } from "./projectSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
+import Container from "@mui/material/Container";
 
-Project.propTypes = {
-    
-};
+Project.propTypes = {};
 
 function Project(props) {
     const dispatch = useDispatch();
-    const [listProject, setListProject] = useState([])
-
-
-    // const abcd = async () => {
-    //     try {
-    //         const response = await dispatch(getListProject())
-    //         const result = unwrapResult(response)
-    //         return result
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
     useEffect(() => {
-        (async () => {
-            const projects = await dispatch(getListProject())
-            const result = unwrapResult(projects)
-            setListProject(result)
-        })()
-    }, [])
+        dispatch(getListProject());
+
+    }, [dispatch])
     return (
         <div>
-            <ProjectPage listProject={listProject}></ProjectPage>
+            <Container>
+                <ProjectPage></ProjectPage>
+            </Container>
         </div>
     );
 }
